4c3886ad7a6bef8eb638944e47114c3eb1019e08
Gonza <gonnza@gmail.com>
2020-03-25 01:37:51 -0300

    Added mobile menu & some animations

--- js/main.js
+++ js/main.js
@@ -1,11 +1,37 @@
 document.addEventListener("DOMContentLoaded", function() {
   setVh();
   collapseNav();
+  var scroll = new SmoothScroll('a[href*="#"]');
   new Glide(".glide", {
     type: "carousel",
     startAt: 0,
     perView: 1
   }).mount();
+
+  let body = document.getElementsByTagName("html")[0];
+
+  const trigger = document.getElementById("menu-trigger");
+  if (trigger) {
+    trigger.addEventListener("click", function() {
+      body.classList.add("menu-opened");
+    });
+  }
+
+  const closeMenu = document.getElementById("close-menu");
+  if (closeMenu) {
+    closeMenu.addEventListener("click", function() {
+      body.classList.remove("menu-opened");
+    });
+  }
+
+  let buttons = document.querySelectorAll("header nav a");
+  if (buttons.length) {
+    buttons.forEach(element => {
+      element.addEventListener("click", () => {
+        body.classList.remove("menu-opened");
+      });
+    });
+  }
 });
 
 window.onscroll = function() {
